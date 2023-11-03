284997473b491f060868c1147810841b7804f8eb
Albano Braz <albano.braz@outlook.com>
2023-11-02 22:06:50 -0300

    first commit and header done

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,13 @@
+import Header from './../components/Header'
+
+function Contact() {
+
+  return (
+    <div className="App">
+        <Header />
+        Contact Page
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Header from './../components/Header'
+
+function Home() {
+
+  return (
+    <div className="App">
+        <Header />
+        Home Page
+    </div>
+  );
+}
+
+export default Home;
